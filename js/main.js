1b5efe8e0a3cb6ed594dd3e6fed403283799bfac
bobur017 <88197833+bobur017@users.noreply.github.com>
2024-02-09 19:18:54 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+const form = document.querySelector('form')
+const okay = document.querySelector('.okay')
+const button = document.querySelector('#btn')
+const password = document.querySelector('#pass')
+const passwordRepeat = document.querySelector('#passRep')
+const name = document.querySelector('#name')
+const passes = document.querySelectorAll('.password')
+
+passes.forEach(input => {
+    const control = input.querySelector('.password-control')
+    const password = input.querySelector('input')
+    
+    control.onclick = (event) => {
+        if (password.getAttribute('type') === 'password') {
+            event.target.classList.add('view')
+            password.setAttribute('type', 'text');
+        } else {
+            event.target.classList.remove('view')
+            password.setAttribute('type', 'password');
+        }
+    }
+})
+
+const postData = async (url, data) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: data
+    })
+    if (response.status === 404) {
+        return alert('Xatolik 404')
+    }
+    if (response.status === 400) {
+        return alert('Bunaqa foydalanuvchi avvaldan mavjud!')
+    }
+    form.style.display = 'none'
+    okay.style.display = 'block'
+    return response.json()
+}
+
+const bindPostData = (form) => {
+    button.onclick = async (event) => {
+        event.preventDefault()
+        const formData = await new FormData(form)
+        const obj = {}
+        await formData.forEach((item, i) => obj[i] = item)
+        obj["password_confirm"] = passwordRepeat.value;
+        const json = await JSON.stringify(obj)
+        if (name.value === '') {
+            return alert('Ismingizni kiriting!')
+        }
+        if (password.value === '') {
+            return alert('Parol kiriting!')
+        }
+        if (password.value.length < 8) {
+            return alert("Eng kamida 8 belgi bo'lishishi shart")
+        }
+        if (passwordRepeat.value === '') {
+            return alert('Parolni qayta kiriting!')
+        }
+        if (password.value === passwordRepeat.value) {
+            console.log(obj);
+            return postData("http://10.0.102.131:8000/api/users/", json)
+        }
+        else {
+            return alert('Parol most emas!')
+        }
+    }
+}
+
+bindPostData(form)
